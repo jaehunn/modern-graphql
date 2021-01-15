432fa03d5eadd762abada8e8976ad87e463a13b5
bangjaehun <qkdwogns98@gmail.com>
2021-01-16 00:02:43 +0900

    es6 module...

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log("Hello GraphQL");
+import myLocation, { msg, name, getGreeting } from "./myModule";
+import sum, { subtract } from "./math";
+
+console.log(`Hello GraphQL, ${msg}, My Name is ${name}`);
+console.log(myLocation);
+console.log(getGreeting(name));
+
+console.log(sum(10, 5));
+console.log(subtract(10, 5));
