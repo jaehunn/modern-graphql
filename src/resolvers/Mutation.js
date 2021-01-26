944fed39070c68261e5a6253655201ffe1bc331a
bangjaehun <qkdwogns98@gmail.com>
2021-01-26 22:50:35 +0900

    mutation update

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -15,6 +15,27 @@ const Mutation = {
 
     return user;
   },
+  updateUser(parent, { id, data }, { db }, info) {
+    const user = db.users.find((user) => user.id === id);
+
+    if (!user) throw new Error(`User not found.`);
+
+    if (typeof data.email === "string") {
+      const emailTaken = db.users.some((user) => user.email === data.email);
+
+      if (emailTaken) throw new Error(`Email taken.`);
+
+      user.email = data.email;
+    }
+
+    if (typeof data.name === "string") user.name = data.name;
+
+    // * 왜 number 체크를 하지않을까요
+    // age is not non-nullable (enable "null")
+    if (typeof data.age !== "undefined") user.age = data.age;
+
+    return user;
+  },
   deleteUser(parent, args, { db }, info) {
     const userIndex = db.users.findIndex((user) => user.id === args.id);
 
@@ -61,6 +82,19 @@ const Mutation = {
 
     return deletedPosts[0];
   },
+  updatePost(parent, { id, data }, { db }, info) {
+    const post = db.posts.find((post) => post.id === id);
+
+    if (!post) throw new Error(`Post not found.`);
+
+    if (typeof data.title === "string") post.title = data.title;
+
+    if (typeof data.body === "string") post.body = data.body;
+
+    if (typeof data.published === "boolean") post.published = data.published;
+
+    return post;
+  },
   createComment(parent, args, { db }, info) {
     const userExists = db.users.some((user) => user.id === args.data.author);
     const postExists = db.posts.some(
@@ -91,6 +125,15 @@ const Mutation = {
     // comment has not relation
 
     return deletedComments[0];
+  },
+  updateComment(parent, { id, data }, { db }, info) {
+    const comment = db.comments.find((comment) => comment.id === id);
+
+    if (!comment) throw new Error(`Comment not found.`);
+
+    if (typeof data.text === "string") comment.text = data.text;
+
+    return comment;
   },
 };
 
