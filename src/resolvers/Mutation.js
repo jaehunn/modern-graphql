9617c65b8f25d7172decaa29ae5ade037f414051
bangjaehun <qkdwogns98@gmail.com>
2021-01-30 22:37:41 +0900

    enum..

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -159,29 +159,46 @@ const Mutation = {
 
     db.comments.push(comment);
 
-    pubsub.publish(`comment ${args.data.post}`, { comment });
+    pubsub.publish(`comment ${args.data.post}`, {
+      comment: {
+        mutation: "CREATED",
+        data: comment,
+      },
+    });
 
     return comment;
   },
-  deleteComment(parent, args, { db }, info) {
+  deleteComment(parent, args, { db, pubsub }, info) {
     const commentIndex = db.comments.findIndex(
       (comment) => comment.id === args.id
     );
 
     if (!~commentIndex) throw new Error(`Comment not found.`);
 
-    const deletedComments = db.comments.splice(commentIndex, 1);
+    const [deletedComment] = db.comments.splice(commentIndex, 1);
 
-    // comment has not relation
+    pubsub.publish(`comment ${deletedComment.post}`, {
+      comment: {
+        mutation: "DELETED",
+        data: deletedComment,
+      },
+    });
 
-    return deletedComments[0];
+    return deletedComment;
   },
-  updateComment(parent, { id, data }, { db }, info) {
+  updateComment(parent, { id, data }, { db, pubsub }, info) {
     const comment = db.comments.find((comment) => comment.id === id);
 
     if (!comment) throw new Error(`Comment not found.`);
 
     if (typeof data.text === "string") comment.text = data.text;
+
+    pubsub.publish(`comment ${comment.post}`, {
+      comment: {
+        mutation: "UPDATED",
+        data: comment,
+      },
+    });
 
     return comment;
   },
